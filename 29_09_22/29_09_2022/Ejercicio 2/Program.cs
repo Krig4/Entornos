94ec9deae7c1b75995ea808f29ccdcf5d38edfa0
Krig4 <113586240+Krig4@users.noreply.github.com>
2022-09-29 09:39:00 +0200

    Add files via upload

---
+++ 29_09_22/29_09_2022/Ejercicio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Dime un numero:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Dime otro numero:");
+        int age2 = Convert.ToInt32(Console.ReadLine());
+        if (age > age2)
+        {
+            Console.WriteLine("El " + age + " es el numero mayor");
+        }
+        else
+        {
+            Console.WriteLine("El " + age2 + " es el numero mayor");
+
+        }
+    }
+}
