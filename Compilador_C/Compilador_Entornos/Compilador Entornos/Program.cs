73069c1cda1b9f44530060769fe53d6b882a4680
Krig4 <113586240+Krig4@users.noreply.github.com>
2022-10-20 09:42:52 +0200

    Add files via upload

---
+++ Compilador_C/Compilador_Entornos/Compilador Entornos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Compilador_Entornos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+            Console.WriteLine("¿Como te llamas?");
+            num = Console.ReadLine();
+            Console.WriteLine("Hola " + num);
+        }
+    }
+}
